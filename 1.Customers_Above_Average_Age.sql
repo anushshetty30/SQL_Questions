dfa92bb89c799d9ddfd84de9847804bebe649440
Anush Shetty <53473042+anushshetty30@users.noreply.github.com>
2026-09-19 19:50:00 +0530

    Update 1.Customers_Above_Average_Age.sql

--- 1.Customers_Above_Average_Age.sql
+++ 1.Customers_Above_Average_Age.sql
@@ -1,4 +1,4 @@
-##Find customers whose age is greater than the average age of all customers.##
+#Find customers whose age is greater than the average age of all customers.
 
 select customer_name, age from customers
 where age > (select avg(age) from customers)

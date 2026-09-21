002945ad7b7ff95cea8116b71c03b0fb7618127e
Anush Shetty <53473042+anushshetty30@users.noreply.github.com>
2026-09-21 16:34:58 +0530

    Update SQL_questions.sql

--- SQL_questions.sql
+++ SQL_questions.sql
@@ -51,3 +51,58 @@ on c.customer_id = o.customer_id
 where status = 'completed'
 group by c.customer_id, c.customer_name
 having total_sales > 8000
+
+#8. City-wise Customer Count
+#Find the number of customers in each city.Sort by customer count from highest to lowest.
+
+SELECT city, COUNT(*) AS customer_count
+FROM customers
+GROUP BY city
+ORDER BY customer_count DESC;
+
+#9. City-wise Sales
+#Calculate the total completed sales for each customer city.
+
+SELECT c.city, SUM(o.amount) AS total_sales
+FROM customers c
+JOIN orders o
+    ON c.customer_id = o.customer_id
+WHERE o.status = 'Completed'
+GROUP BY c.city
+ORDER BY total_sales DESC;
+
+#10. Average Order Value by Status
+#Find the average order amount for each order status.
+
+SELECT status, AVG(amount) AS average_order_value
+FROM orders
+GROUP BY status;
+
+#Date & Business Analysis
+  
+#11. Year-Month Sales
+#Calculate total completed sales for each year-month.
+
+SELECT
+    DATE_FORMAT(order_date, '%Y-%m') AS month_date,
+    SUM(amount) AS total_sales
+FROM orders
+WHERE status = 'Completed'
+GROUP BY DATE_FORMAT(order_date, '%Y-%m')
+ORDER BY month_date;
+
+#12. Monthly Order Count
+#Find the number of completed orders for each month.
+
+select date_format(order_date,'%Y-%m'), count(order_id) as total_orders from orders
+where status = 'completed'
+group by date_format(order_date,'%Y-%m')
+
+13. Highest Sales Month
+Find the month with the highest total completed sales.
+
+select date_format(order_date,'%Y-%m') as month_date, sum(amount) as total_sales from orders
+where status = 'completed'
+group by date_format(order_date,'%Y-%m')
+order by total_sales desc
+limit 1

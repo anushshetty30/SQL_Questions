f4ed36049838bbeaf39c4fc6ef0825426666719c
Anush Shetty <53473042+anushshetty30@users.noreply.github.com>
2026-09-22 19:38:54 +0530

    Update SQL_Questions_2.sql

--- SQL_Questions_2.sql
+++ SQL_Questions_2.sql
@@ -1,1 +1,13 @@
+#1. Customer Purchase Frequency
+#Find each customer's total number of orders and total completed sales.
 
+  SELECT c.customer_id, customer_name, COUNT(o.order_id) AS total_orders,
+    SUM( CASE 
+            WHEN status = 'completed' THEN o.amount
+            ELSE 0
+        END
+    ) AS total_sales
+FROM customers c 
+INNER JOIN orders o
+    ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, customer_name;

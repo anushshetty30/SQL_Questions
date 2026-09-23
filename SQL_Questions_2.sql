d17ddbb57acef4a727ac6212a07e3ab5e9a056ca
Anush Shetty <53473042+anushshetty30@users.noreply.github.com>
2026-09-23 20:04:53 +0530

    Update SQL_Questions_2.sql

--- SQL_Questions_2.sql
+++ SQL_Questions_2.sql
@@ -11,3 +11,14 @@ FROM customers c
 INNER JOIN orders o
     ON c.customer_id = o.customer_id
 GROUP BY c.customer_id, customer_name;
+
+#2. Average Spend Per Customer
+#Calculate the average completed sales per customer. Consider only customers who have made at least one completed order.
+
+select avg(total_sales) AS average_spend
+from (
+select customer_id, sum(amount) as total_sales
+from orders
+where status = 'completed'
+group by customer_id
+) as customer_sales
